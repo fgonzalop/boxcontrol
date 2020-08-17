f3c24f1a0d3c51be4a7a601193d60dc1192cadc3
fgonzalop <fgonzalo.palacios@gmail.com>
2020-08-17 20:37:56 +0200

    Callback on_message funcionando.

--- BoxDomotic.py
+++ BoxDomotic.py
@@ -8,6 +8,13 @@ from RF24Mesh import *
 
 from struct import unpack
 
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+    
+    
 mqttHost = "127.0.0.1"
 topic = [['data/RF24/Node0Lux', 'data/RF24/Node0Tra'], ['data/RF24/Node1Lux', 'data/RF24/Node1Tra']]
 #data = 24
@@ -22,11 +29,18 @@ mesh.setNodeID(0)
 mesh.begin()
 radio.setPALevel(RF24_PA_MAX) # Power Amplifier
 radio.printDetails()
+
 print("Starting BoxDomotic...")
+
+mqttc = mqtt.Client()
+mqttc.on_message = on_message;
+mqttc.connect(mqttHost, 1883)
+mqttc.subscribe("calefaccion")
 
 while 1:
     mesh.update()
     mesh.DHCP()
+    mqttc.loop_start()
 
     while network.available():
         header, payload = network.read(10)
@@ -35,9 +49,10 @@ while 1:
         else:
             print("Rcv bad type {} from 0{:o}".format(header.type,header.from_node));
             print("{}".format(mesh.getNodeID(header.from_node)))
-            mqttc = mqtt.Client()
-            mqttc.connect(mqttHost, 1883)
+            
+
             data = unpack("L",payload)[0]
             print("{}".format(topic[mesh.getNodeID(header.from_node)][header.type]))
             mqttc.publish(topic[mesh.getNodeID(header.from_node)][header.type], data)
             mqttc.loop(2)
+    mqttc.loop_stop()
