e960acca5426724c21dd3755381894abae35c5fe
fgonzalop <fgonzalo.palacios@gmail.com>
2022-07-29 00:51:46 +0200

    Eliminación de PRINTS.

--- scripts_python/Registros_SUN2000.py
+++ scripts_python/Registros_SUN2000.py
@@ -27,97 +27,97 @@ time.sleep(1)
 if client.connect():
     # The trick is use  unit_id=1 not in ModbusClient() but in client.read_holding_registers()
     request = client.read_holding_registers(address=30070,count= 1, unit=0)
-    print (request.registers[0]) #300
+    #print (request.registers[0]) #300
     #print (request.registers[1])
 else:
     print ('something is wrong with IP or port')
 
-    
-request = client.read_holding_registers(address=30071,count= 1, unit=0)
-print (request.registers[0])
+while (True):    
+    request = client.read_holding_registers(address=30071,count= 1, unit=0)
+    #print (request.registers[0])
+    topic = "inverter/pv_string"
+    clientmqtt.publish(topic, request.registers[0]);
 
-topic = "inverter/pv_string"
-clientmqtt.publish(topic, request.registers[0]);
+    request = client.read_holding_registers(address=32016,count= 1, unit=0)
+    #print (request.registers[0])
+    topic = "inverter/pv_1/voltage"
+    clientmqtt.publish(topic, request.registers[0]);
 
-request = client.read_holding_registers(address=32016,count= 1, unit=0)
-print (request.registers[0])
-topic = "inverter/pv_1/voltage"
-clientmqtt.publish(topic, request.registers[0]);
+    request = client.read_holding_registers(address=32017,count= 1, unit=0)
+    #print (request.registers[0])
+    topic = "inverter/pv_1/current"
+    clientmqtt.publish(topic, request.registers[0]);
 
-request = client.read_holding_registers(address=32017,count= 1, unit=0)
-print (request.registers[0])
-topic = "inverter/pv_1/current"
-clientmqtt.publish(topic, request.registers[0]);
+    request = client.read_holding_registers(address=32018,count= 1, unit=0)
+    #print (request.registers[0])
+    topic = "inverter/pv_2/voltage"
+    clientmqtt.publish(topic, request.registers[0]);
 
-request = client.read_holding_registers(address=32018,count= 1, unit=0)
-print (request.registers[0])
-topic = "inverter/pv_2/voltage"
-clientmqtt.publish(topic, request.registers[0]);
+    request = client.read_holding_registers(address=32019,count= 1, unit=0)
+    #print (request.registers[0])	
+    topic = "inverter/pv_2/current"
+    clientmqtt.publish(topic, request.registers[0]);
 
-request = client.read_holding_registers(address=32019,count= 1, unit=0)
-print (request.registers[0])	
-topic = "inverter/pv_2/current"
-clientmqtt.publish(topic, request.registers[0]);
+    request = client.read_holding_registers(address=32000,count= 1, unit=0)
+    #print (request.registers[0])
+    value="--"	
+    if (request.registers[0] == 0) :
+        value="TBD"
+    if (request.registers[0] == 1) :
+        value="standby"  
 
-request = client.read_holding_registers(address=32000,count= 1, unit=0)
-print (request.registers[0])
-value="--"	
-if (request.registers[0] == 0) :
-    value="TBD"
-if (request.registers[0] == 1) :
-    value="standby"  
+    topic = "inverter/status"
+    clientmqtt.publish(topic, value);
 
-topic = "inverter/status"
-clientmqtt.publish(topic, value);
+    #request = client.read_holding_registers(address=32085,count= 1, unit=0)
+    #print (request.registers[0])
 
-#request = client.read_holding_registers(address=32085,count= 1, unit=0)
-#print (request.registers[0])
+    #print("Grid current")
+    request = client.read_holding_registers(address=32072,count= 2, unit=0)
+    #print (request.registers[0])
+    #print (request.registers[1])
+    topic = "inverter/solar_current"
+    clientmqtt.publish(topic, float(request.registers[0]*0x10000+request.registers[1])/1000.0 );
 
-print("Grid current")
-request = client.read_holding_registers(address=32072,count= 2, unit=0)
-print (request.registers[0])
-print (request.registers[1])
-topic = "inverter/solar_current"
-clientmqtt.publish(topic, float(request.registers[0]*0x10000+request.registers[1])/1000.0 );
+    #print("Grid METER current")
+    request = client.read_holding_registers(address=37107,count= 2, unit=0)
+    #print (request.registers[0])
+    #print (request.registers[1])
+    #print (int(0x10000*request.registers[0]+request.registers[1]), type(request.registers[0]))
+    m=int(0x10000*request.registers[0]+request.registers[1]+1)
+    #print (ctypes.c_int32(~m).value)
+    topic = "inverter/grid/current"
+    clientmqtt.publish(topic, ctypes.c_int32(~m).value);
 
-print("Grid METER current")
-request = client.read_holding_registers(address=37107,count= 2, unit=0)
-print (request.registers[0])
-print (request.registers[1])
-print (int(0x10000*request.registers[0]+request.registers[1]), type(request.registers[0]))
-m=int(0x10000*request.registers[0]+request.registers[1]+1)
-print (ctypes.c_int32(~m).value)
-topic = "inverter/grid/current"
-clientmqtt.publish(topic, ctypes.c_int32(~m).value);
+    #request = client.read_holding_registers(address=32089,count= 1, unit=0)
+    #print (request.registers[0])
 
-#request = client.read_holding_registers(address=32089,count= 1, unit=0)
-#print (request.registers[0])
+    request = client.read_holding_registers(address=37113,count= 2, unit=0)
+    #print (request.registers[0])
+    #print (request.registers[1])
+    m=int(0x10000*request.registers[0]+request.registers[1]+1)
+    #print (ctypes.c_int32(~m).value)
+    topic = "inverter/grid/power"
+    clientmqtt.publish(topic, ctypes.c_int32(~m).value);
 
-request = client.read_holding_registers(address=37113,count= 2, unit=0)
-print (request.registers[0])
-print (request.registers[1])
-m=int(0x10000*request.registers[0]+request.registers[1]+1)
-print (ctypes.c_int32(~m).value)
-topic = "inverter/grid/power"
-clientmqtt.publish(topic, ctypes.c_int32(~m).value);
+    request = client.read_holding_registers(address=37101,count= 2, unit=0)
+    #print (request.registers[0])
+    #print (request.registers[1])
+    topic = "inverter/grid/voltage"
+    clientmqtt.publish(topic, request.registers[1]/10.0)
 
-request = client.read_holding_registers(address=37101,count= 2, unit=0)
-print (request.registers[0])
-print (request.registers[1])
-topic = "inverter/grid/voltage"
-clientmqtt.publish(topic, request.registers[1]/10.0)
+    #print("input power")
+    request = client.read_holding_registers(address=32064,count= 2, unit=0)
+    #print (request.registers[0])
+    #print (request.registers[1])
+    topic = "inverter/solar/power"
+    clientmqtt.publish(topic, request.registers[1]);
 
-print("input power")
-request = client.read_holding_registers(address=32064,count= 2, unit=0)
-print (request.registers[0])
-print (request.registers[1])
-topic = "inverter/solar/power"
-clientmqtt.publish(topic, request.registers[1]);
+    #print("Grid voltage")
+    request = client.read_holding_registers(address=32066,count= 1, unit=0)
+    #print (request.registers[0])
+    topic = "inverter/solar/voltage"
+    clientmqtt.publish(topic, request.registers[0]/10.0)
 
-print("Grid voltage")
-request = client.read_holding_registers(address=32066,count= 1, unit=0)
-print (request.registers[0])
-topic = "inverter/solar/voltage"
-clientmqtt.publish(topic, request.registers[0]/10.0)
-
+    time.sleep(1)
 
